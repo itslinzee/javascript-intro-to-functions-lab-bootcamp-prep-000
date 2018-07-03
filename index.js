4d1b44434e4fa3a630ca189374bb71ffc2d52eb7
Lindsay Ortiz <lindsay.ortiz@live.com>
2018-07-03 20:49:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function whisper(string) {
 function logShout(string) {
   console.log(string)
 }
+
+function logWhisper(string) {
+  console.log(string)
+}
+
+function sayHiToGrandma(string) {
+  if(string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  else if(string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  else if(string === "I love you, Grandma") {
+    return "I love you, too."
+  }
+}
